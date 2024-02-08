ea795ddf033dd9f72887b3afee440a8ee1e41a3b
Harish-muthyala <91313039+Harish-muthyala@users.noreply.github.com>
2024-02-08 13:02:07 +0530

    Update executorservic.java

--- executorservic.java
+++ executorservic.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.Callable;
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
